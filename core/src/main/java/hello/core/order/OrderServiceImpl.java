e6a6bff81d5cd3f42c6ab30b0e0bf117abed6224
icekimchi <pjh010828@gmail.com>
2024-01-30 17:36:02 +0900

    할인정책추가

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,17 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements  OrderService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private  DiscountPolicy discountPolicy; //인터페이스에만 의존하도록 코드 변경
+
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //구체 클래스인 FixDiscountPolicy에도 의존하고 있다.
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberID, String itemName, int itemPrice) {
